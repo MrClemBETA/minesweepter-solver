ecc481dac34686547a4680a3f7b706964d5d2bff
Anthony Clemetson <aclemetson@cousd.net>
2021-11-19 10:25:49 -0800

    Pushing a gitignore and clicker.

---
+++ Minesweeper Solver/Controller/MouseEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Minesweeper_Solver.Controller
+{
+    class MouseEvents
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+
+        private const UInt32 MOUSEEVENT_LEFTDOWN = 0x0002;
+        private const UInt32 MOUSEEVENT_LEFTUP = 0x0004;
+
+        public static void SendClick()
+        {
+            mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, new System.IntPtr());
+            mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, new System.IntPtr());
+        }
+    }
+}
---
+++ Minesweeper Solver/Program.cs
@@ -0,0 +1,13 @@
+﻿using Minesweeper_Solver.Controller;
+using System;
+
+namespace Minesweeper_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MouseEvents.SendClick();
+        }
+    }
+}
